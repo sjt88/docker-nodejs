c48e6307451915dcac81e6fa74509a072fa1b728
sjt88 <sjt88@users.noreply.github.com>
2016-02-10 15:22:47 +0000

    adds dependencies

--- app/models.js
+++ app/models.js
@@ -3,7 +3,7 @@ module.exports = {
   login: function (data) {
     if (!data.username) return {err: true, msg: 'no username'}
     if (!data.password) return {err: true, msg: 'no password'}
-    return { msg: 'login ok!', ok: true}
+    return {msg: 'login ok!', ok: true}
   },
   getProfile: function (data) {
     if (!data.loggedIn) return {err: true, msg: 'not logged in :('}
